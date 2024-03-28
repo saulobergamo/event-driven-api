60fc1bd064558db1e04dd05ffd70c3d32a0daa4a
saulo.bergamo <saulo.bergamo@dextra-sw.com>
2024-03-28 02:25:52 -0300

    add docker-compose.yml and README to project

--- cmds/build-sonar.sh
+++ cmds/build-sonar.sh
@@ -3,7 +3,7 @@
 SONAR_URL="http://localhost:9000"
 SONAR_LOGIN="28468fb8a8bc98e17f6dc981eae0a87c563e2d51"
 SONAR_BRANCH="localtest"
-SONAR_PROJECT_NAME="sistemas-distribuidos-waiter"
+SONAR_PROJECT_NAME="event-driven-api-pub1"
 
 ./gradlew clean sonarqube build \
     -Dsonar.projectKey=${SONAR_PROJECT_NAME}
